aa9876e6c990d6b80ca601150dd4c4c110b650cf
Aleksey <meysonsib200189@mail.ru>
2022-09-03 12:51:52 +0700

    начало работы с фрагментами тема оружие

---
+++ app/src/main/java/com/hfad/new_lesson_fragment_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.new_lesson_fragment_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_activity_main);
+    }
+}
