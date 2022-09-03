354e08125622fbdb63e59821fcefca7cff6ed067
Aleksey <meysonsib200189@mail.ru>
2022-09-03 13:20:42 +0700

    создал фрагменты и контейнеры для размещения списка оружия (каркас готов)

--- app/src/main/java/com/hfad/new_lesson_fragment_app/MainActivity.java
+++ app/src/main/java/com/hfad/new_lesson_fragment_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfad.new_lesson_fragment_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_activity_main);
+
+        if (savedInstanceState == null) {
+            GunsListFragment gunsListFragment = new GunsListFragment();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.gun, gunsListFragment).commit();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                GunFragment gunFragment = new GunFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.detail_gun, gunFragment).commit();
+            }
+        }
+
     }
 }
